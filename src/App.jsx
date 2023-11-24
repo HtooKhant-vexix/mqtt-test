e39a899b866d80053c8f5a751f18220be0853adc
det-frontend <131895424+det-frontend@users.noreply.github.com>
2023-11-24 10:13:15 +0630

    h

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,42 @@ const App = () => {
   const [msg, setMsg] = useState();
   const [publishValue, setPublicValue] = useState();
   const [subscribeValue, setSubscribeValue] = useState();
-  const [topicValue, setTopicValue] = useState("testtopic");
-  const [topicV, setTopicV] = useState("testtopic");
+  const [topicValue, setTopicValue] = useState();
+  const [topicV, setTopicV] = useState();
   const [msgArr, setMsgArr] = useState([]);
   const [chg, setChg] = useState(true);
   const [handleMsg, setHandleMsg] = useState();
+  const [broker, setBroker] = useState();
 
   const client = mqtt.connect(
     "wss://htookhant:Admin123@f6ce8c16ab1f4b958a2179d249d62bf3.s2.eu.hivemq.cloud:8884/mqtt"
   );
 
   useEffect(() => {
+    client.on("connect", () => {
+      if (topicValue == "device") {
+        console.log("topic is d");
+
+        client.subscribe("device", (err) => {
+          err && console.log(err);
+        });
+      } else if (topicValue == "testtopic") {
+        console.log("topic is t");
+        client.subscribe("testtopic", (err) => {
+          err && console.log(err);
+        });
+      }
+    });
+  }, [topicValue]);
+
+  useEffect(() => {
     const handleMessage = (topic, message) => {
+      console.log("====================================");
+      console.log(topic);
+      console.log("====================================");
       const text = message.toString();
-      setHandleMsg(text);
       setMsgArr((prevMsgArr) => [...prevMsgArr, text]);
     };
-
-    client.on("connect", () => {
-      client.subscribe(topicValue, (err) => {
-        err && console.log(err);
-      });
-    });
 
     client.on("message", handleMessage);
 
@@ -35,7 +49,11 @@ const App = () => {
       client.end(); // Cleanup: Ensure client is closed when the component unmounts
       client.removeListener("message", handleMessage); // Remove the listener on unmount
     };
-  }, []);
+  }, [topicValue]);
+
+  useEffect(() => {
+    setMsgArr([]);
+  }, [broker]);
 
   const publishHandler = (topic, value) => {
     client.publish(topic, value);
@@ -88,10 +106,45 @@ const App = () => {
           />
         </div>
       </div>
-      <div className=" shadow-2xl bg-white h-[80vh] w-[600px] overflow-scroll shadow-green-500/30 p-6 pt-7 flex items-start gap-3 rounded-md flex-col">
-        {msgArr.map((e) => (
-          <div className="bg-slate-200 inline px-5 rounded-full py-3">{e}</div>
-        ))}
+      <div className=" shadow-2xl bg-white h-[80vh] w-[600px]  shadow-green-500/30 p-6 pt-7 flex items-start gap-3 rounded-md flex-col">
+        <div className=" ms-2 text-xl font-semibold font-mono text-gray-500">
+          Subscription
+        </div>
+        <div className=" flex gap-2">
+          <button
+            onClick={() => {
+              setBroker("testtopic"), setTopicValue("testtopic");
+            }}
+            className={
+              broker == "testtopic"
+                ? "bg-green-500 text-white rounded-full  px-3 py-2 "
+                : "bg-transparent border-green-500 text-green-500 border rounded-full  px-3 py-2 "
+            }
+          >
+            testtopic
+          </button>
+          <button
+            onClick={() => {
+              setBroker("device"), setTopicValue("device");
+            }}
+            // className="bg-transparent border-green-500 text-green-500 border rounded-full  px-3 py-2 "
+            className={
+              broker == "device"
+                ? "bg-green-500 text-white rounded-full  px-3 py-2 "
+                : "bg-transparent border-green-500 text-green-500 border rounded-full  px-3 py-2 "
+            }
+            // className="bg-transparent border-green-500 text-green-500 border rounded-full  px-3 py-2 "
+          >
+            device
+          </button>
+        </div>
+        <div className="overflow-scroll flex flex-col items-start gap-2 w-full h-full pt-5">
+          {msgArr.map((e) => (
+            <div className="bg-slate-200 inline px-5 rounded-full py-3">
+              {e}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
